3fa1a79835c444302709736fb63def0d0cf3234f
Noah Boos <noahboos-dev@proton.me>
2026-09-20 23:02:45 +0200

    Chore - Setup event handling.

--- src/main/java/fr/noahboos/rockified/Rockified.java
+++ src/main/java/fr/noahboos/rockified/Rockified.java
@@ -1,5 +1,6 @@
 package fr.noahboos.rockified;
 
+import fr.noahboos.rockified.events.RockifiedEvents;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.Identifier;
@@ -20,8 +21,9 @@ public class Rockified implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Initializing {}.", MOD_ID);
+		RockifiedEvents.initialize();
+		LOGGER.info("Initialized {}.", MOD_ID);
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/fr/noahboos/rockified/events/RockifiedEvents.java
@@ -0,0 +1,11 @@
+package fr.noahboos.rockified.events;
+
+import fr.noahboos.rockified.Rockified;
+
+public class RockifiedEvents {
+    public static void initialize() {
+        Rockified.LOGGER.info("Registering {}'s events.", Rockified.MOD_ID);
+        //
+        Rockified.LOGGER.info("Registered {}'s events.", Rockified.MOD_ID);
+    }
+}
